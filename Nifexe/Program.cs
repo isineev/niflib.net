d50a2c32de15dbfc15823ef8149859ba51ef9dac
isineev <isineev@gmail.com>
2020-04-28 11:49:03 +0700

    test executable

--- Nifexe/Program.cs
+++ Nifexe/Program.cs
@@ -6,6 +6,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -15,18 +16,31 @@ namespace Nifexe
     {
         static void Main(string[] args)
         {
+            if (args.Length == 2)
+            {
+                try
+                {
+                    var fileStream = File.OpenRead(args[0]);
+                    using var binaryReader = new BinaryReader(fileStream);
+                    new NiFile(binaryReader).Save(args[1]);
 
-            try
-            {
-                var srcPath = args[0];
-                var dstPath = args[1];
-                using var br = new BinaryReader(File.OpenRead(srcPath));
-                var niFile = new NiFile(br);
-                niFile.Save(dstPath);
+                    var hash0 = SHA1.Create().ComputeHash(File.ReadAllBytes(args[0])).Select(b => $"{b:x2}").Aggregate("", (r, i) => r + i);
+                    var hash1 = SHA1.Create().ComputeHash(File.ReadAllBytes(args[1])).Select(b => $"{b:x2}").Aggregate("", (r, i) => r + i);
+
+                    Console.WriteLine($"Source file: {args[0]}");
+                    Console.WriteLine($"SHA1={hash0}");
+                    Console.WriteLine($"Target file: {args[1]}");
+                    Console.WriteLine($"SHA1={hash1}");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
             }
-            catch (Exception e)
+            else
             {
-                Console.WriteLine(e);
+                Console.WriteLine("Usage: nifexe <file-to-read> <file-to-save>");
             }
         }
     }
